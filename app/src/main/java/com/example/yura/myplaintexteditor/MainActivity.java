9f4f50e411b2386d06a8acbf1ee60723348777d6
Yuriy Ivanov <yuriygr@yahoo.com>
2018-10-17 23:53:58 +0300

    commit

---
+++ app/src/main/java/com/example/yura/myplaintexteditor/MainActivity.java
@@ -0,0 +1,184 @@
+package com.example.yura.myplaintexteditor;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Константы для меню
+    public static final int IDM_OPEN = 101;
+    public static final int IDM_SAVE = 102;
+    public static final int IDM_PREF = 103;
+
+    private final static String FILENAME = "sample.txt"; // имя файла
+    private EditText mEditText;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mEditText = (EditText) findViewById(R.id.editText);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        menu.add(Menu.NONE, IDM_OPEN, Menu.NONE, "Открыть");
+
+        menu.add(Menu.NONE, IDM_SAVE, Menu.NONE, "Сохранить")
+                .setIcon(android.R.drawable.ic_menu_save);
+
+        menu.add(Menu.NONE, IDM_PREF, Menu.NONE, "Настройки");
+
+        return(super.onCreateOptionsMenu(menu));
+    }
+*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_open:
+                openFile(FILENAME);
+                return true;
+            case R.id.action_save:
+                saveFile(FILENAME);
+                return true;
+            case R.id.action_settings:
+                Intent intent = new Intent();
+                intent.setClass(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return true;
+        }
+    }
+
+
+/*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case IDM_OPEN:
+                openFile(FILENAME);
+                return true;
+            case IDM_SAVE:
+                saveFile(FILENAME);
+                return true;
+            case IDM_PREF:
+                Intent intent = new Intent();
+                intent.setClass(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return true;
+        }
+    }
+*/
+////////////////////////////////
+
+    // Метод для открытия файла
+    private void openFile(String fileName) {
+        try {
+            InputStream inputStream = openFileInput(fileName);
+
+            if (inputStream != null) {
+                InputStreamReader isr = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(isr);
+                String line;
+                StringBuilder builder = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line + "\n");
+                }
+
+                inputStream.close();
+                mEditText.setText(builder.toString());
+            }
+        } catch (Throwable t) {
+            Toast.makeText(getApplicationContext(),
+                    "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Метод для сохранения файла
+    private void saveFile(String fileName) {
+        try {
+            OutputStream outputStream = openFileOutput(fileName, 0);
+            OutputStreamWriter osw = new OutputStreamWriter(outputStream);
+            osw.write(mEditText.getText().toString());
+            osw.close();
+        } catch (Throwable t) {
+            Toast.makeText(getApplicationContext(),
+                    "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        // читаем установленное значение из CheckBoxPreference
+        if (prefs.getBoolean(getString(R.string.pref_openmode), false)) {
+            openFile(FILENAME);
+        }
+        // читаем размер шрифта из EditTextPreference
+        float fSize = Float.parseFloat(prefs.getString(
+                getString(R.string.pref_size), "20"));
+// применяем настройки в текстовом поле
+        mEditText.setTextSize(fSize);
+        // читаем стили текста из ListPreference
+        String regular = prefs.getString(getString(R.string.pref_style), "");
+        int typeface = Typeface.NORMAL;
+
+        if (regular.contains("Полужирный"))
+            typeface += Typeface.BOLD;
+
+        if (regular.contains("Курсив"))
+            typeface += Typeface.ITALIC;
+
+// меняем настройки в EditText
+        mEditText.setTypeface(null, typeface);
+///////////////////////////////////////////////////////////////
+        // читаем цвет текста из CheckBoxPreference
+        // и суммируем значения для получения дополнительньк цветов текста
+        int color = 0;
+        if (prefs.getBoolean(getString(R.string.pref_color_black), false))
+            color += Color.BLACK;
+        if (prefs.getBoolean(getString(R.string.pref_color_red), false))
+            color += Color.RED;
+        if (prefs.getBoolean(getString(R.string.pref_color_green), false))
+            color += Color.GREEN;
+        if (prefs.getBoolean(getString(R.string.pref_color_blue), false))
+            color += Color.BLUE;
+
+        mEditText.setTextColor(color);
+
+
+    }
+}
